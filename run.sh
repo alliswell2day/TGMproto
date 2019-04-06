1637402e86c433dcee12d7756bc14c830428b176
alliswell2day <frainzy1477@gmail.com>
2019-04-06 22:59:25 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -94,4 +94,4 @@ echo "[*]   Make sure to fix the links in case you run the proxy on a different 
 echo
 echo '[+] Starting proxy...'
 sleep 1
-exec /usr/local/bin/mtproto-proxy -p 2398 -H 443 -M "$WORKERS" -C 60000 --aes-pwd /etc/telegram/proxy-secret -u root $CONFIG --allow-skip-dh --nat-info "$INTERNAL_IP:$IP" $SECRET_CMD $TAG_CMD
+exec /usr/local/bin/mtproto-proxy -p 2398 -H 9000 -M "$WORKERS" -C 60000 --aes-pwd /etc/telegram/proxy-secret -u root $CONFIG --allow-skip-dh --nat-info "$INTERNAL_IP:$IP" $SECRET_CMD $TAG_CMD
